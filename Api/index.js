028888db171568b534f29a0c6ebf8fc4da4d58e9
LautaroBernati <73679349+LautaroBernati@users.noreply.github.com>
2021-11-03 01:55:03 -0300

    all

--- Api/index.js
+++ Api/index.js
@@ -41,6 +41,47 @@ app.use(async function (req, res, next) {
     res.sendFile(__dirname + '/views/index.html');
 }); */
 
+app.post('/usuarios', (req, res) => {
+    req.body.save((rr,usuario)=>res.status(200).send(usuario))
+    
+
+    
+    //Usuario.insertMany(req.body);
+    //console.log(req.body.email);
+
+    /* if(!req.body.name || !req.body.email) {
+        respuesta = {
+         error: true,
+         codigo: 502,
+         mensaje: 'El campo name y email son requeridos'
+        };
+       } else {
+        if(req.body.name !== '' || req.body.email !== '') {
+         respuesta = {
+          error: true,
+          codigo: 503,
+          mensaje: 'El usuario ya fue creado previamente'
+         };
+        } else {
+            usuario = {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+         };
+         respuesta = {
+          error: false,
+          codigo: 200,
+          mensaje: 'Usuario creado',
+          respuesta: usuario
+         };
+        }
+       }
+       
+       res.send(respuesta); */
+       
+    
+});    
+
 app.get('/usuarios', function (req, res) {  //endpoint, ruta. Siempre solo una respuesta por path.
     console.log('entre');
     Usuario.find().select(["email","name"]).then(data => {
@@ -50,8 +91,6 @@ app.get('/usuarios', function (req, res) {  //endpoint, ruta. Siempre solo una r
             console.log(err);
             res.status(404).end(); // enviar error
         });
-
-
 });
 
 app.get('/usuarios/:id', function (req, res) {  //endpoint, ruta. Siempre solo una respuesta por path.
